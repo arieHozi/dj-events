9cf4cdac4c9c20ad5efe655b348ee4cc8923371a
ahozi <ahozias@gmail.com>
2022-05-14 22:01:07 +0300

    add header fotter

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import { Layout } from '../components/Layout'
+
+
+export default function AboutPage() {
+  return (
+    <Layout title="abuot dfdsfsd">
+      <h1>About</h1>
+      <p>This ia an app to find the latest DJ and other musical events</p>
+      <p>Version:1.0.0</p>
+    </Layout>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+
+import { Layout } from "../components/Layout";
+
+export default function HomePage() {
+  return (
+    <Layout >
+      <h1>home</h1>
+    </Layout>
+  )
+}
