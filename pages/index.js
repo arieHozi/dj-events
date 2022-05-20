e96d9c4bfc8b922def4c6f1bacf13edc77a19808
arik hozias <ahozias@gmail.com>
2022-05-20 23:30:36 +0300

    add routs

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ import { Layout } from "@/components/Layout";
 export default function HomePage() {
   return (
     <Layout title="Home" >
-      <h1>home</h1>
+      <h1>Upcoming events</h1>
+
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch()
 }
